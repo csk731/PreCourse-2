44967cbcb472d632eb1db1460e38d529520f637f
Chaitanya Allu <chaitanyaa1999@gmail.com>
2024-12-25 23:04:04 -0500

    solved Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-class QuickSort 
+class Exercise_2 
 { 
     /* This function takes last element as pivot, 
        places the pivot element at its correct 
@@ -7,21 +7,39 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+        //Your code here 
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp; 
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+        //Write code here for Partition and Swap 
+        int pivot = arr[high];
+        int i = low - 1, j = low;
+        while (j < high) {
+            if (arr[j] <= pivot) swap(arr, ++i, j);
+            j++;
+        }
+        swap(arr, ++i, j);
+        return i;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
-    {  
-            // Recursively sort elements before 
-            // partition and after partition 
+    {
+        if (low >= high) {
+            return;
+        }
+        // Recursively sort elements before 
+        // partition and after partition 
+        int p = partition(arr, low, high);
+
+        sort(arr, low, p - 1);
+        sort(arr, p + 1, high);
     } 
   
     /* A utility function to print array of size n */
@@ -39,7 +57,7 @@ class QuickSort
         int arr[] = {10, 7, 8, 9, 1, 5}; 
         int n = arr.length; 
   
-        QuickSort ob = new QuickSort(); 
+        Exercise_2 ob = new Exercise_2(); 
         ob.sort(arr, 0, n-1); 
   
         System.out.println("sorted array"); 
